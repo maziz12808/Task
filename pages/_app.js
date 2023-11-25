a1319229d54b211b0924d595b9ac33f5b1fe641d
aziz12808 <maziz12808@gmail.com>
2023-11-25 17:55:34 +0500

    create staging branch and install require library

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import '../styles/globals.css'
+import 'animate.css';
+import 'boxicons/css/boxicons.min.css'
+import 'swiper/css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
