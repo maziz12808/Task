7f0142eaf41e5db2af1f17ad220b7ad47026e43f
aziz12808 <maziz12808@gmail.com>
2023-12-08 12:00:56 +0500

    category page created

---
+++ components/admin/category/index.jsx
@@ -0,0 +1,10 @@
+import LayoutEl from "../shared/layout"
+const Category = ()=>{
+    return (
+        <LayoutEl>
+            <h1>category</h1>
+        </LayoutEl>
+    )
+}
+
+export default Category
