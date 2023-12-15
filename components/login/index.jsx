66a1a6980166035e1a115b6cce17d2f5eb7f98fa
aziz12808 <maziz12808@gmail.com>
2023-12-15 15:45:21 +0500

    added breadcrumb

--- components/login/index.jsx
+++ components/login/index.jsx
@@ -1,7 +1,21 @@
-import Layout from "../shared/layout"
+import Layout from "../shared/layout";
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import {RightOutlined} from "@ant-design/icons";
+import { Breadcrumb } from "antd";
+
 const Login = ()=>{
+    const pathname = usePathname();
     return (
         <Layout>
+            <Breadcrumb 
+                className=" capitalize border-b px-[3.6%] py-4"
+                items={[
+                    {title: <Link href="/">Home</Link>},  
+                    {title: " Login-Registration"}
+                ]}
+                separator={<RightOutlined style={{fontSize: '11px'}} />}
+            />
             <div className="px-[3.6%]">
                 <h2>Login</h2>
             </div>
