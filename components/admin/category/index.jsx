050c8734f9ac78e31d9e0fe3988e038775608e47
aziz12808 <maziz12808@gmail.com>
2023-12-17 17:25:07 +0500

    category ui created

--- components/admin/category/index.jsx
+++ components/admin/category/index.jsx
@@ -1,8 +1,115 @@
+import { useState,useRef } from "react"
 import LayoutEl from "../shared/layout"
+import { 
+    Button,
+    Drawer, 
+    Form,
+    Input,
+    Select,
+    Divider,
+    Space
+} from "antd"
+import {PlusOutlined } from "@ant-design/icons"
+
+const { Item } = Form;
 const Category = ()=>{
+    const [openDrawer,setOpenDrawer] = useState(false);
+    const [items, setItems] = useState([]);
+    const [name, setName] = useState('');
+    const inputRef = useRef(null);
+    const toolbar = [
+        <Button 
+            type="text" 
+            className="font-semibold"  
+            style={{backgroundColor: "#4FC9DA",borderRadius:0,color:"white"}} 
+            onClick={()=> setOpenDrawer(!openDrawer)}
+        >
+            Add Category
+        </Button>
+    ]
+    const showDrawer = () => {
+        setOpenDrawer(true);
+      };
+      const onClose = () => {
+        setOpenDrawer(false);
+      };
+      const onNameChange = (event) => {
+        setName(event.target.value);
+      };
+      const addItem = (e) => {
+        e.preventDefault();
+        if(name=='') return alert("please enter category name") 
+        if(items.indexOf(name) !== -1) return (setName(''),alert("Category Already Exist"))
+        setItems([...items, name]);
+        setName('');
+        setTimeout(() => {
+          inputRef.current?.focus();
+        }, 0);
+      };
+    const onCategory = (val)=>{
+        console.log(val);
+    }
     return (
-        <LayoutEl>
-            <h1>category</h1>
+        <LayoutEl toolbar={toolbar}>
+            <Drawer title="Basic Drawer" placement="right" onClose={onClose} open={true}>
+                <Form onFinish={onCategory}>
+                    <Item 
+                        name="catetitlegory" 
+                        rules={
+                            [
+                                {
+                                    required: true,
+                                    message: "Category name is required!"
+                                }
+                            ]
+                        }
+                    >
+                        <Select 
+                            size="large"
+                            placeholder="Select Category"
+                            dropdownRender={(menu) => (
+                                <>
+                                    <Divider
+                                        style={{
+                                        margin: '8px 0',
+                                        }}
+                                    />
+                                    <Space>
+                                        <Input
+                                            placeholder="Please enter category name"
+                                            ref={inputRef}
+                                            value={name}
+                                            onChange={onNameChange}
+                                            onKeyDown={(e) => e.stopPropagation()} 
+                                        />
+                                        <Button 
+                                            type="text" 
+                                            size="middle" 
+                                            icon={<PlusOutlined />} 
+                                            onClick={addItem} 
+                                        >
+                                            Add item
+                                        </Button>
+                                    </Space>
+                                    {menu}
+                                </>
+                            )}
+                            options={items.map((item) => {
+                                return {
+                                    label: item,
+                                    value: item,
+                                }
+                            })}
+                        />
+                    </Item>
+                    <Item name="childrenCategory">
+                        <Input size="large" />
+                    </Item>
+                    <Item>
+                        <Button htmlType="submit">Submit</Button>
+                    </Item>
+                </Form>
+            </Drawer>
         </LayoutEl>
     )
 }
--- components/admin/shared/layout/index.jsx
+++ components/admin/shared/layout/index.jsx
@@ -1,40 +1,46 @@
 import { DashboardOutlined, MenuOutlined } from "@ant-design/icons"
-import { Button, Layout,Menu } from "antd"
+import { Button, Layout,Menu,ConfigProvider,theme } from "antd"
+import Head from "next/head"
 import Link from "next/link"
 import { useState } from "react"
 
 const {Sider,Content,Header} = Layout
 
-const LayoutEl = ({children})=>{
+const LayoutEl = ({children,title=null,toolbar=null})=>{
     const [open,setOpen] = useState(false)
     const menu = [
         {
-            label: 'Dasbboard',
-            key: '/dashboard',
+            label: <Link href="/admin">Dasbboard</Link>,
+            key: 'dashboard',
             icon: <DashboardOutlined />,
         },
         {
             label: <Link href="/admin/category">Category</Link>,
-            key: '/category',
+            key: 'category',
             icon: <i className='bx bx-category' />,
         }
     ]
     return (
         <Layout>
+            <Head>
+                <title>{ title && title}</title>
+            </Head>
             <Sider 
                 trigger={null} 
                 collapsed={open} 
                 collapsible 
-                className="min-h-screen" 
-                theme="light"
+                className="min-h-screen admin-sider" 
             >
-                <Menu items={menu} />
+                <Menu items={menu}  />
             </Sider>
             <Layout>
-                <Header className="bg-gray-200">
+                <Header className="bg-white flex justify-between items-center px-4">
                     <Button type="text" icon={<MenuOutlined />} onClick={()=> setOpen(!open)} />
+                    <div className="flex gap-x-4">
+                        {toolbar && toolbar}
+                    </div>
                 </Header>
-                <Content className="p-4">
+                <Content className="p-4 bg-[#FDFCFB] border-t">
                     {
                         children
                     }
